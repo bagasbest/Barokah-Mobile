f19aade2e266c7db41926c3a0fdbf0dd3463b1b2
bagasbest <45277197+bagasbest@users.noreply.github.com>
2022-06-13 03:56:21 +0700

    calculate & hpp

--- app/src/main/java/com/project/barokahmobile/ui/hpp_telur/HppEggFragment.kt
+++ app/src/main/java/com/project/barokahmobile/ui/hpp_telur/HppEggFragment.kt
@@ -1,14 +1,22 @@
 package com.project.barokahmobile.ui.hpp_telur
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.project.barokahmobile.R
-import com.project.barokahmobile.databinding.FragmentDatabaseBinding
 import com.project.barokahmobile.databinding.FragmentHppEggBinding
+import java.text.DecimalFormat
 
 
 class HppEggFragment : Fragment() {
@@ -28,6 +36,99 @@ class HppEggFragment : Fragment() {
             .into(binding.image)
 
         return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.submitBtn.setOnClickListener {
+            formValidation()
+        }
+
+    }
+
+    private fun formValidation() {
+        val combineFeed = binding.combineFeed.text.toString().trim()
+        val eggProduction = binding.eggProduction.text.toString().trim()
+        val feedPrice = binding.feedPrice.text.toString().trim()
+        val otherPrice = binding.otherPrice.text.toString().trim()
+
+        if (combineFeed.isEmpty() || combineFeed.toInt() <= 0) {
+            Toast.makeText(activity, "Jumlah campuran pakan dalam 1 hari, minimal 1 kg", Toast.LENGTH_SHORT).show()
+        } else if (eggProduction.isEmpty() || eggProduction.toInt() <= 0) {
+            Toast.makeText(activity, "Jumlah telur yang dihasilkan 1 hari, minimal 1 kg", Toast.LENGTH_SHORT).show()
+        } else if (feedPrice.isEmpty() || feedPrice.toInt() < 0) {
+            Toast.makeText(activity, "Harga campuran pakan per kg tidak boleh kosong dan negatif", Toast.LENGTH_SHORT).show()
+        } else if (otherPrice.isEmpty() || otherPrice.toLong() < 0) {
+            Toast.makeText(
+                activity,
+                "Biaya lain - lain tidak boleh kosong dan negatif",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+
+            showDialogPopupResult(
+                combineFeed.toDouble(),
+                eggProduction.toDouble(),
+                feedPrice.toDouble(),
+                otherPrice.toDouble(),
+            )
+
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showDialogPopupResult(
+        combineFeed: Double,
+        eggProduction: Double,
+        feedPrice: Double,
+        otherPrice: Double,
+    ) {
+
+        val title: TextView
+        val dismissBtn: Button
+        val hppLL: LinearLayout
+        val combineFeedTv: TextView
+        val eggProductionTv: TextView
+        val feedPriceTv: TextView
+        val otherPriceTv: TextView
+        val price2Tv: TextView
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.popup_result)
+
+        title = dialog.findViewById(R.id.editText)
+        dismissBtn = dialog.findViewById(R.id.dismissBtn)
+        hppLL = dialog.findViewById(R.id.hpp)
+        combineFeedTv = dialog.findViewById(R.id.combineFeed)
+        eggProductionTv = dialog.findViewById(R.id.eggProduction)
+        feedPriceTv = dialog.findViewById(R.id.feedPrice)
+        otherPriceTv = dialog.findViewById(R.id.otherPrice)
+        price2Tv = dialog.findViewById(R.id.price2)
+
+        hppLL.visibility = View.VISIBLE
+
+        val formatter = DecimalFormat("#,###")
+        title.text = "Hasil Harga Pokok Produksi (HPP)\nTelur per Kilogram"
+        combineFeedTv.text = "Jumlah campuran pakan dalam 1 hari : $combineFeed kg"
+        eggProductionTv.text = "Jumlah telur yang dihasilkan 1 hari : $eggProduction kg"
+        feedPriceTv.text = "Harga campuran pakan : $feedPrice per kg"
+        otherPriceTv.text = "Biaya lain - lain : $otherPrice per kg"
+
+
+        val fcr : Double = combineFeed / eggProduction
+        val formula : Double = ((feedPrice * fcr) + otherPrice)
+        price2Tv.text = "Rp${formatter.format(formula)} per kilogram"
+
+
+        dismissBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+
+
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
 
     }
 
--- app/src/main/java/com/project/barokahmobile/ui/menghitung_pakan/CalculatePakanFragment.kt
+++ app/src/main/java/com/project/barokahmobile/ui/menghitung_pakan/CalculatePakanFragment.kt
@@ -1,17 +1,25 @@
 package com.project.barokahmobile.ui.menghitung_pakan
 
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.project.barokahmobile.R
 import com.project.barokahmobile.authentication.LoginActivity
 import com.project.barokahmobile.databinding.FragmentCalculatePakanBinding
-import com.project.barokahmobile.databinding.FragmentInfoPakanBinding
+import java.text.DecimalFormat
 
 
 class CalculatePakanFragment : Fragment() {
@@ -47,6 +55,111 @@ class CalculatePakanFragment : Fragment() {
             startActivity(intent)
             activity?.finish()
         }
+
+        binding.submitBtn.setOnClickListener {
+            formValidation()
+        }
+    }
+
+    private fun formValidation() {
+        val concentrate = binding.concentrate.text.toString().trim()
+        val corn = binding.corn.text.toString().trim()
+        val katul = binding.katul.text.toString().trim()
+        val concentratePrice = binding.concentratePrice.text.toString().trim()
+        val cornPrice = binding.cornPrice.text.toString().trim()
+        val katulPrice = binding.katulPrice.text.toString().trim()
+
+        if (concentrate.isEmpty() || concentrate.toInt() <= 0) {
+            Toast.makeText(activity, "Jumlah konsentrat minimal 1 kg", Toast.LENGTH_SHORT).show()
+        } else if (corn.isEmpty() || corn.toInt() <= 0) {
+            Toast.makeText(activity, "Jumlah Jagung minimal 1 kg", Toast.LENGTH_SHORT).show()
+        } else if (katul.isEmpty() || katul.toInt() <= 0) {
+            Toast.makeText(activity, "Jumlah Katul minimal 1 kg", Toast.LENGTH_SHORT).show()
+        } else if (concentratePrice.isEmpty() || concentratePrice.toLong() < 0) {
+            Toast.makeText(
+                activity,
+                "Harga konsentrat per kg tidak boleh kosong dan negatif",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else if (cornPrice.isEmpty() || cornPrice.toLong() < 0) {
+            Toast.makeText(activity, "Harga Jagung per kg tidak boleh kosong dan negatif", Toast.LENGTH_SHORT)
+                .show()
+        } else if (katulPrice.isEmpty() || katulPrice.toLong() < 0) {
+            Toast.makeText(activity, "Harga katul per kg tidak boleh kosong dan negatif", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+
+            showDialogPopupResult(
+                concentrate.toDouble(),
+                corn.toDouble(),
+                katul.toDouble(),
+                concentratePrice.toDouble(),
+                cornPrice.toDouble(),
+                katulPrice.toDouble(),
+            )
+
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showDialogPopupResult(
+        concentrate: Double,
+        corn: Double,
+        katul: Double,
+        concentratePrice: Double,
+        cornPrice: Double,
+        katulPrice: Double
+    ) {
+
+        val title: TextView
+        val dismissBtn: Button
+        val calculateLL: LinearLayout
+        val concentrateTotalTv: TextView
+        val cornTotalTv: TextView
+        val katulTotalTv: TextView
+        val concentratePriceTv: TextView
+        val cornPriceTv: TextView
+        val katulPriceTv: TextView
+        val price1Tv: TextView
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.popup_result)
+
+        title = dialog.findViewById(R.id.editText)
+        dismissBtn = dialog.findViewById(R.id.dismissBtn)
+        calculateLL = dialog.findViewById(R.id.calculate)
+        concentrateTotalTv = dialog.findViewById(R.id.concentrateTotal)
+        cornTotalTv = dialog.findViewById(R.id.cornTotal)
+        katulTotalTv = dialog.findViewById(R.id.katulTotal)
+        concentratePriceTv = dialog.findViewById(R.id.concentratePrice)
+        cornPriceTv = dialog.findViewById(R.id.cornPrice)
+        katulPriceTv = dialog.findViewById(R.id.katulPrice)
+        price1Tv = dialog.findViewById(R.id.price)
+
+        calculateLL.visibility = View.VISIBLE
+
+        val formatter = DecimalFormat("#,###")
+        title.text = "Hasil Harga Campuran\nPakan Ayam per kilogram"
+        concentrateTotalTv.text = "Jumlah Konsentrat : $concentrate"
+        cornTotalTv.text = "Jumlah jagung : $corn"
+        katulTotalTv.text = "Jumlah katul : $katul"
+        concentratePriceTv.text = "Harga konsentrat : $concentratePrice per kg"
+        cornPriceTv.text = "Harga jagung : $concentratePrice per kg"
+        katulPriceTv.text = "Harga katul : $concentratePrice per kg"
+
+
+        val formula : Double = ((concentrate * concentratePrice) + (corn * cornPrice) + (katul * katulPrice)) / (concentrate + corn + katul)
+        price1Tv.text = "Rp${formatter.format(formula)}"
+
+
+        dismissBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+
+
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+
     }
 
     override fun onDestroy() {
